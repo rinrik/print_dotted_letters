f978c9d3e4288601c53358c88b6ee71c88756b18
Rinat Khissamov <rinat.khissamov@agcsoft.com>
2024-05-08 19:19:48 +0300

    decrease number of spaces

--- src/printer/print.ts
+++ src/printer/print.ts
@@ -9,11 +9,11 @@ export function printDotWord(word: string) {
 
         letters.forEach(letter => {
                 let addToOutput = allowedLetters.getValue(letter)
-                output.letter.lineOne = output.letter.lineOne.concat(addToOutput.dottedLetter.letter.lineOne, "    ")
-                output.letter.lineTwo = output.letter.lineTwo.concat(addToOutput.dottedLetter.letter.lineTwo, "    ")
-                output.letter.lineThree = output.letter.lineThree.concat(addToOutput.dottedLetter.letter.lineThree, "    ")
-                output.letter.lineFour = output.letter.lineFour.concat(addToOutput.dottedLetter.letter.lineFour, "    ")
-                output.letter.lineFive = output.letter.lineFive.concat(addToOutput.dottedLetter.letter.lineFive, "    ")
+                output.letter.lineOne = output.letter.lineOne.concat(addToOutput.dottedLetter.letter.lineOne, " ")
+                output.letter.lineTwo = output.letter.lineTwo.concat(addToOutput.dottedLetter.letter.lineTwo, " ")
+                output.letter.lineThree = output.letter.lineThree.concat(addToOutput.dottedLetter.letter.lineThree, " ")
+                output.letter.lineFour = output.letter.lineFour.concat(addToOutput.dottedLetter.letter.lineFour, " ")
+                output.letter.lineFive = output.letter.lineFive.concat(addToOutput.dottedLetter.letter.lineFive, " ")
         })
 
         let outputStrings = Object.values(output.letter)
